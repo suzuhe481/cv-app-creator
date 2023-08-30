2a487f190cf3809839113a37517909407b2f3b59
Hector <baseballhector@gmail.com>
2023-08-30 16:53:45 -0400

    Displays the form and preview. Passes functions and state variables to children.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 import "./App.css";
+import Form from "./components/Form/Form";
+import Preview from "./components/Preview/Preview";
 
 function App() {
-  return <></>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <Form
+        onFirstNameChange={handleFirstNameChange}
+        onLastNameChange={handleLastNameChange}
+      />
+      <Preview fName={firstName} lName={lastName} />
+    </>
+  );
 }
 
 export default App;
