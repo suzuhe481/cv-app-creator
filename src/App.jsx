02ad00601316398b5f2a0ed556357a93da862d9f
Hector <baseballhector@gmail.com>
2023-08-31 14:58:53 -0400

    Combined states and handlers for form data into 1 useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,25 @@ import Form from "./components/Form/Form";
 import Preview from "./components/Preview/Preview";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [location, setLocation] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    location: "",
+  });
 
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
-
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
-
-  function handlePhoneChange(e) {
-    setPhone(e.target.value);
-  }
-
-  function handleLocationChange(e) {
-    setLocation(e.target.value);
+  function handleFormChange({ type, data }) {
+    setFormData((prev) => ({
+      ...prev,
+      [type]: data.target.value,
+    }));
   }
 
   return (
     <>
-      <Form
-        onFirstNameChange={handleFirstNameChange}
-        onLastNameChange={handleLastNameChange}
-        onEmailChange={handleEmailChange}
-        onPhoneChange={handlePhoneChange}
-        onLocationChange={handleLocationChange}
-      />
-      <Preview
-        firstName={firstName}
-        lastName={lastName}
-        email={email}
-        phone={phone}
-        location={location}
-      />
+      <Form onFormChange={handleFormChange} />
+      <Preview formData={formData} />
     </>
   );
 }
