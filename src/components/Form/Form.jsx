ec25a7ea7c01171e1ca003b4d97b56fe36634510
Hector <baseballhector@gmail.com>
2023-08-31 17:00:24 -0400

    Added section for experience

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -45,12 +45,53 @@ function Form({ onFormChange }) {
           onChange={(data) => onFormChange({ type: "location", data })}
         />
       </fieldset>
+
       <fieldset className="summary">
         <legend>Summary</legend>
+        <label htmlFor="Summary"></label>
         <textarea
           id="summary"
           name="summary"
           onChange={(data) => onFormChange({ type: "summary", data })}
+          rows="4"
+        />
+      </fieldset>
+
+      <fieldset className="experience">
+        <legend>Experience</legend>
+        <label htmlFor="jobTitle">Title</label>
+        <input
+          id="jobTitle"
+          type="text"
+          name="jobTitle"
+          onChange={(data) => onFormChange({ type: "jobTitle", data })}
+        />
+        <label htmlFor="company">Company</label>
+        <input
+          id="company"
+          type="text"
+          name="company"
+          onChange={(data) => onFormChange({ type: "company", data })}
+        />
+        <label htmlFor="startDate">Start Date</label>
+        <input
+          id="startDate"
+          type="date"
+          name="startDate"
+          onChange={(data) => onFormChange({ type: "startDate", data })}
+        />
+        <label htmlFor="endDate">End Date</label>
+        <input
+          id="endDate"
+          type="date"
+          name="endDate"
+          onChange={(data) => onFormChange({ type: "endDate", data })}
+        />
+        <label htmlFor="responsibilities">Responsibilities</label>
+        <textarea
+          id="responsibilities"
+          name="responsibilities"
+          onChange={(data) => onFormChange({ type: "responsibilities", data })}
           rows="4"
         />
       </fieldset>
